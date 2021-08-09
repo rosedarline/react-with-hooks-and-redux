3a9146152196bd5ce6ca993beec8f29082ab3195
Rose Adam <rosedarline@gmail.com>
2021-08-09 15:38:09 -0400

    Add readme. Update bundle name.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/app.js",
     output: {
         path: path.join(__dirname, "public"),
-        filename: "boundle.js"
+        filename: "bundle.js"
     },
     module:{
         rules: [{
